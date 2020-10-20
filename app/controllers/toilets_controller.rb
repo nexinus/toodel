a2dcea11c78c7dc0af9d0bb77fa7fb0975eea6d3
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-20 14:50:28 +0200

    Added banner

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,10 +1,22 @@
 class ToiletsController < ApplicationController
   def index
+    @toilets = Toilet.all
   end
 
   def new
+    @toilet = Toilet.new
   end
 
   def create
+    @toilet = Toilet.new(toilet_params)
+    @toilet.save
+
+    redirect_to toilets_path(@toilet)
+  end
+
+  private
+
+  def toilet_params
+    params.require(:toilet).permit(:name, :description)
   end
 end
