ffba1fd73e79e5acc2a6f66f75f6630137d669d7
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-28 18:52:36 +0100

    Edited main layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Toilet.destroy_all
+
+puts "Creating toilets..."
+10.times do
+  toilet = Toilet.new(
+    name:    Faker::Company.name,
+    description:  Faker::Lorem.sentence,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}"
+  )
+  toilet.save!
+end
+puts "Finished!"
