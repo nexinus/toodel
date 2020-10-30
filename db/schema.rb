1ee819beaf02e8de8b3922656b7e045d014e8c8b
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-30 09:16:19 +0100

    Redo bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_074415) do
+ActiveRecord::Schema.define(version: 2020_10_30_081538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_30_074415) do
   create_table "bookings", force: :cascade do |t|
     t.date "date"
     t.time "time"
-    t.boolean "confirmed"
+    t.string "confirmed"
     t.bigint "toilet_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
