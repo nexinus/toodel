87e3c06210ad316a919ab618ddc941cca2c1ab65
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-11-03 15:27:24 +0100

    Added photo preview

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { previewImageOnFileSelect } from '../components/photo_preview';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initUpdateNavbarOnScroll();
+  previewImageOnFileSelect();
 });
