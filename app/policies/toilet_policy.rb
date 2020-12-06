76f6c6000de37423eba4430a15483805c5f860ec
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-12-06 16:36:50 +0100

    Edited toilet policy

--- app/policies/toilet_policy.rb
+++ app/policies/toilet_policy.rb
@@ -5,7 +5,19 @@ class ToiletPolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    true # Anyone can view a toilet
+  end
+
   def create?
-    return true
+    return true # Anyone can create a toilet
+  end
+
+  def update?
+    record.user == user # Only toilet creator can update it
+  end
+
+  def destroy
+    record.user == user # Only toilet creator can destroy it
   end
 end
