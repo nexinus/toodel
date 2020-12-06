d40b06746e3259f34204c460e930634393867a98
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-12-06 16:28:58 +0100

    Edited pundit application policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -11,7 +11,8 @@ class ApplicationPolicy
   end
 
   def show?
-    false
+    # false
+    scope.where(:id => record.id).exists?
   end
 
   def create?
@@ -32,6 +33,10 @@ class ApplicationPolicy
 
   def destroy?
     false
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
   end
 
   class Scope
