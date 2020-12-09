5b433f293b38820e627a1897d555d52f6062520b
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-12-09 20:57:02 +0100

    Config Geocoder until fit map to markers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { previewImageOnFileSelect } from '../components/photo_preview';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initUpdateNavbarOnScroll();
   previewImageOnFileSelect();
+  initMapbox();
 });
