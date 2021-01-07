cd6429d68a7a27e8f06342d2dc9a2ec87e9d5ee6
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2021-01-07 21:36:49 +0100

    Map box tweeked

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -2,9 +2,8 @@ class ToiletsController < ApplicationController
   before_action :set_toilet, only: [:show, :edit, :update, :destroy]
 
   def index
-    @toilets = policy_scope(Toilet).order.where.not(created_at: :desc, latitude: nil, longitude: nil)
-    # @toilets = policy_scope(Toilet).where.not(latitude: nil, longitude: nil)
-    # @toilets = Toilet.where.not(latitude: nil, longitude: nil)
+    # @toilets = policy_scope(Toilet).order.where.not(created_at: :desc, latitude: nil, longitude: nil)
+    @toilets = policy_scope(Toilet).where.not(latitude: nil, longitude: nil)
 
     # the `geocoded` scope filters only toilets with coordinates (latitude & longitude)
     @markers = @toilets.geocoded.map do |toilet|
