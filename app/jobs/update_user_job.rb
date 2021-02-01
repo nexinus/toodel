d9ea82ee7c60d7573c49a6cee2ad45c9c0585fe5
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2021-02-01 20:21:28 +0100

    Reinstated Sidekiq

--- app/jobs/update_user_job.rb
+++ app/jobs/update_user_job.rb
@@ -1,10 +1,10 @@
-# class UpdateUserJob < ApplicationJob
-#   queue_as :default
+class UpdateUserJob < ApplicationJob
+  queue_as :default
 
-#   def perform(user_id)
-#     user = User.find(user_id)
-#     puts "Updating user #{user.first_name}..."
-#     sleep 2
-#     puts "Done! Updated #{user.first_name} details..."
-#   end
-# end
+  def perform(user_id)
+    user = User.find(user_id)
+    puts "Updating user #{user.first_name}..."
+    sleep 2
+    puts "Done! Updated #{user.first_name} details..."
+  end
+end
