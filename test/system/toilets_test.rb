1b5a4ee7cbc39b3bd7d26ad2975e7332cbc12a40
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2021-02-04 15:57:19 +0100

    Added all test yml including Travis

---
+++ test/system/toilets_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ToiletsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit toilets_url
+  #
+  #   assert_selector "h1", text: "Toilet"
+  # end
+end
