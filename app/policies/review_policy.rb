6cece9ca05207f7bb26f4addd802c9e0ad9363fe
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2021-02-23 17:23:23 +0100

    Minor edit on view

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -6,22 +6,25 @@ class ReviewPolicy < ApplicationPolicy
   end
 
   def show?
-    true # Anyone can view a toilet
+    true # Anyone can view a review
   end
 
   def create?
-    # true # Anyone can create a toilet
-    !user.nil? # Any logged in user can create a toilet
+    # true # Anyone can create a review
+    !user.nil? # Any logged in user can create a review
+    # !record.user == user # Toilet owner cannot create own review
+    # - record: the review passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
   end
 
   def update?
-    record.user == user # Only toilet creator can update it
-    # - record: the toilet passed to the `authorize` method in controller
+    record.user == user # Only review creator can update it
+    # - record: the review passed to the `authorize` method in controller
     # - user:   the `current_user` signed in with Devise.
   end
 
   def destroy?
-    # record.user == user # Only toilet creator can destroy it
+    # record.user == user # Only review creator can destroy it
     update? # same logic
   end
 end
